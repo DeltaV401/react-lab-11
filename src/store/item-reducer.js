4fff1904a58d8e85d7c1cfc9ecd2102f55c9ed1d
Jon Struve <ocdammo3@gmail.com>
2019-11-07 15:27:50 -0600

    Added toggle details method to redux item reducer

--- src/store/item-reducer.js
+++ src/store/item-reducer.js
@@ -1,9 +1,16 @@
-import { initialState } from "./todolist-reducer";
-
 export const DETAILS = 'details';
+
+export const initialState = {};
 
 export function reducer(state = initialState, action = {}) {
   switch(action.type) {
+    case TOGGLE_DETAILS: {
+      let item = state.todoList.find(item => item._id === action.payload);
+        return {
+          details: item || {},
+          showDetails: !!item,
+        }
+      }
     default:
       return state;
   }
