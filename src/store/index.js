393f66af4eea4dea72f3e6a7cd9c8edf7472859f
Jon Struve <ocdammo3@gmail.com>
2019-11-07 16:31:34 -0600

    Added form to redux

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { reducer as todoList } from './todolist-reducer';
 import { reducer as details } from './item-reducer';
+import { reducer as form } from 'redux-form'
 
 let reducer = combineReducers({
   todoList,
   details,
+  form,
 });
 
 export default function() {
